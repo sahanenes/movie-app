c6c55cbf3ec7b60cb81aa1b86c0ba916860bf2c2
sahenenes <sahanenesss@gmail.com>
2022-11-09 16:13:19 +0300

    iframe added

--- src/components/VideoSection.jsx
+++ src/components/VideoSection.jsx
@@ -1,7 +1,18 @@
 import React from "react";
 
-const VideoSection = () => {
-  return <div>VideoSection</div>;
+const VideoSection = ({ videoKey }) => {
+  return (
+    <div className="w-10/12 md:w-3/5 mx-auto my-3">
+      <div className="ratio ratio-16x9">
+        <iframe
+          className="rounded-xl"
+          src={`https://www.youtube.com/embed/${videoKey}?autoplay=1&mute=1`}
+          title="YouTube video"
+          allowFullScreen
+        ></iframe>
+      </div>
+    </div>
+  );
 };
 
 export default VideoSection;
